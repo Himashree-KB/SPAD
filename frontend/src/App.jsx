b2ba885ad026800572b1e3d80bbff190a91dd618
nakulmanoj <nakulmanoj.dev@gmail.com>
2025-09-13 11:35:16 +0530

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer' 
+
 import Home from './pages/Home'
 import About from './pages/About'
 import Impact from './pages/Impact'
+import Involved from './pages/Involved'
 import Donate from './pages/Donate'
 import Contact from './pages/Contact'
 
-
-
-
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#FBFFF1]">
-      
-      <main className="max-w-[1100px] mx-auto px-5 py-8">
+    <div className="min-h-screen flex flex-col bg-[#FBFFF1]">
+      {/* Navbar should stay at the top */}
+      <Navbar />
+
+      {/* Main content */}
+      <main className="flex-grow max-w-[1100px] mx-auto px-5 py-8">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/impact" element={<Impact />} />
+          <Route path="/involved" element={<Involved />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
+
+      {/* Footer stays at the bottom */}
+      <Footer />
     </div>
   )
 }
